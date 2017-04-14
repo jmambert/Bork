0b7e6fc731325d9ab506345f98f1f2638d59a72e
Jordan <Jordan@10.213.180.50>
2017-04-14 17:27:19 -0400

    LightEvent and LockedEvent added but not implemented

---
+++ src/bork1/LockedEvent.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bork1;
+
+/**
+ *
+ * @author Jordan
+ */
+class LockedEvent extends Event {
+    
+    LockedEvent() {
+        
+    }
+    
+    public String execute() {
+        
+        return "Sorry, this exit is locked.";
+    }
+    
+}
